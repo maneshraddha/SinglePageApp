2e8fd61e8fd17bfa7eb4b9781eec1332f7946713
maneshraddha <maneshraddha1006>
2016-12-12 17:07:47 +0530

    Single page app json and firebase

---
+++ Single_Page_App_firebase/assets/js/config.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCgDct4AoMfG91Lbe7Me6Uk4UQ4S7lz_Vo",
+    authDomain: "country-aa248.firebaseapp.com",
+    databaseURL: "https://country-aa248.firebaseio.com",
+    storageBucket: "country-aa248.appspot.com",
+    messagingSenderId: "429873485235"
+  };
+  firebase.initializeApp(config);
